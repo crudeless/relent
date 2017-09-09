013458a02a866699ba0a4fa5ddd22729a74b156c
crudeless <jonathanesbjug@gmail.com>
2017-09-09 23:52:39 +0200

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+<nav class="navbar">
+    <section>
+        <button class="navbar-link">Button</button>
+    </section>
+</nav>
+</template>
+
+<style lang="scss">
+
+$color-primary: #f44336;
+$color-primary-light: #ff7961;
+$color-primary-dark: #ba000d;
+.navbar {
+    position: fixed;
+    top:0;
+    height: 80px;
+    width: 100%;
+    background-color: $color-primary;
+}
+
+.navbar-link {
+    min-height: 100%;
+}
+</style>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <navbar>--</navbar>
+    <h1>Text</h1>
+  </section>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
